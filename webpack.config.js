b286001d62992126c5e3ca7384e12e49d98a26b5
adammertel <12932677+adammertel@users.noreply.github.com>
2024-08-14 12:36:53 +0200

    Update packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "docs"),
   },
   module: {
     rules: [
@@ -12,55 +16,55 @@ module.exports = {
         test: /\.js$|\.jsx$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.html$/,
         use: [
           {
-            loader: 'html-loader',
-            options: { minimize: true }
-          }
-        ]
+            loader: "html-loader",
+            options: { minimize: true },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.scss$/,
         loaders: [
-          'style-loader',
-          { loader: 'css-loader', options: { importLoaders: 1 } },
-          'sass-loader'
-        ]
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "sass-loader",
+        ],
       },
       {
         test: /\.(jpe?g|png|gif|svg|eot|ttf|svg|woff|woff2|ico)$/i,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: './assets/',
-              publicPath: './assets/'
-            }
-          }
-        ]
-      }
-    ]
+              name: "[name].[ext]",
+              outputPath: "./assets/",
+              publicPath: "./assets/",
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-      favicon: './src/assets/favicon.ico'
+      template: "./src/index.html",
+      filename: "./index.html",
+      favicon: "./src/assets/favicon.ico",
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css'
+      filename: "[name].css",
+      chunkFilename: "[id].css",
     }),
-    new CopyWebpackPlugin([{ from: './src/data', to: 'data' }], {})
-  ]
+    new CopyWebpackPlugin([{ from: "./data", to: "data" }], {}),
+  ],
 };
